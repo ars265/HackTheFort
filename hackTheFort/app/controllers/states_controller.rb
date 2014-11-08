949388c7607b955f4732ca487229b2ed4d0ae6be
Anthony Sparks <anthony.sparks@enspiresoftware.com>
2014-11-08 14:56:01 -0500

    Added base files for state index

---
+++ hackTheFort/app/controllers/states_controller.rb
@@ -0,0 +1,57 @@
+
+    class StatesController < ApplicationController
+      # GET /states
+      # GET /states.json
+      def index
+        @states = State.all
+
+        render states_path
+      end
+
+      # GET /states/1
+      # GET /states/1.json
+      def show
+        @state = State.find(params[:id])
+
+        render json: @state
+      end
+
+      # POST /states
+      # POST /states.json
+      def create
+        @state = State.new(state_params)
+
+        if @state.save
+          render json: @state, status: :created, location: @state
+        else
+          render json: @state.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /states/1
+      # PATCH/PUT /states/1.json
+      def update
+        @state = State.find(params[:id])
+
+        if @state.update(state_params)
+          head :no_content
+        else
+          render json: @state.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /states/1
+      # DELETE /states/1.json
+      def destroy
+        @state = State.find(params[:id])
+        @state.destroy
+
+        head :no_content
+      end
+
+      private
+
+      def state_params
+        params[:state]
+      end
+    end
